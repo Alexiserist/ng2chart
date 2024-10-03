de81306d31269df3d9ccbc52e68f1945d5765cd8
Chanchat.T <76992641+Alexiserist@users.noreply.github.com>
2024-10-03 21:42:58 +0700

    ng2-charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartConfiguration, ChartOptions } from "chart.js";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chartjs';
+  
+  lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+   lineChartOptions: ChartOptions<'line'> = {
+    responsive: false
+  };
+   lineChartLegend = true;
+
 }
